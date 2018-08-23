0ffb3ea35b163161bc2a1b052fd7cb835b79b143
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-23 11:32:14 +0530

    Create 11.java

---
+++ 11.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		  Scanner sc=new Scanner(System.in);
+	    String s=sc.next();
+	    switch(s)
+	    {
+	    	  case "Sunday":
+	    		System.out.println("yes");
+	    		break;
+	    		case "Monday":
+	    		System.out.println("no");
+	    		break;
+	    		case "Tuesday":
+	    		System.out.println("no");
+	    		break;
+	    		case "Wednesday":
+	    		System.out.println("no");
+	    		break;
+	    		case "Thursday":
+	    		System.out.println("no");
+	    		break;
+	    		case "Firday":
+	    		System.out.println("no");
+	    		break;
+	    		case "Saturday":
+	    		System.out.println("yes");
+	    		break;
+	    }
+	
+	}
+}
