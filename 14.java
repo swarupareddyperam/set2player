a404e479edc4bec4fd1e4429c149e3bb656f9f38
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-23 12:13:08 +0530

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+
+public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String str=sc.next();
+		int f=0;
+		String s=str.replaceAll("[aeiouAEIOU]", "");
+    String reverse="";
+     for ( int i =s.length() - 1 ; i >= 0 ; i-- )
+     reverse = reverse + s.charAt(i);
+     System.out.println(reverse);
+     }
+     }            
